3742e9429bdbc12dd1a71150aaf42b80c8ffd59d
DaniekTj <imperio.hacker@gmail.com>
2021-02-02 03:46:12 -0800

    Formulario de contacto v2

---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+    const $form = document.querySelector(".contact__form");
+    const $loader = document.querySelector(".contact-form-loader");
+    const $response = document.querySelector(".contact-form-response");
+  
+    $form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      $loader.classList.remove("none");
+      fetch("https://formsubmit.co/ajax/daniektj@gmail.com", {
+        method: "POST",
+        body: new FormData(e.target),
+      })
+        .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+        .then((json) => {
+          console.log(json);
+          location.hash = "#gracias";
+          $form.reset();
+        })
+        .catch((err) => {
+          console.log(err);
+          let message =
+            err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+          $response.querySelector(
+            "h3"
+          ).innerHTML = `Error ${err.status}: ${message}`;
+        })
+        .finally(() => {
+          $loader.classList.add("none");
+          setTimeout(() => {
+            location.hash = "#close";
+          }, 3000);
+        });
+    });
